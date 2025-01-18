8bdd3907a5672bd23ec75a9e59fec984146a3bd5
Roman Movchan <roman.movchan@protonmail.com>
2025-01-18 08:25:46 +0200

    Updated sun shader, background z-index experiments

--- scenes/background/scripts/background.gd
+++ scenes/background/scripts/background.gd
@@ -11,8 +11,20 @@ func _ready():
 	for layer in layers_container.get_children():
 		if layer is BackgroundLayer:
 			_layers.append(layer)
+	_setup_z_indexes_for_layers()
 
 func _process(_delta):
 	var shift = _camera.position
 	for layer in _layers:
 		layer.shift(shift, _camera.zoom)
+
+func _setup_z_indexes_for_layers():
+	var dict: Dictionary [float, BackgroundLayer] = {}
+	for layer in _layers:
+		dict[layer.distance] = layer
+	dict.sort()
+	var i := 0
+	for layer in dict:
+		dict[layer].z_index = -i
+		i += 1
+	
--- scenes/background/scripts/background_layer.gd
+++ scenes/background/scripts/background_layer.gd
@@ -1,5 +1,5 @@
 class_name BackgroundLayer
-extends Node
+extends Node2D
 
 @export var distance: float = 0.0
 
